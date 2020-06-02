3d8e766ec17105aa09be9f3843e3ce68acc0f785
xdvrx1 <mongAlvarez@gmail.com>
2020-06-02 20:10:20 +0800

    Initial commit

---
+++ SpringBootProjectDir/src/main/java/xdvrx1_serverProject/SpringMainMethod.java
@@ -0,0 +1,16 @@
+package xdvrx1_serverProject;
+
+import java.io.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static java.lang.System.exit;
+
+@SpringBootApplication
+public class SpringMainMethod {
+  
+    public static void main(String[] args) {
+	SpringApplication.run(SpringMainMethod.class, args);
+    }
+}
